e6d4aa48f072f8b8eb44dd8f3221e5d5e75c66d8
André Luis Bonfim <andrebonfim@pm.me>
2025-12-29 23:08:26 -0300

    Config: Atualizando gitignore e ajustes no main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
+import pandas as pd
 from docx import Document
 
 def extrair_dados():
   pasta = "./documentos"
+  # Lista vazia para guardar os dados de cada arquivo
+  lista_dados = []
 
   if not os.path.exists(pasta):
-    print("Pasta não encontrada!")
+    print(f" A pasta '{pasta}' não existia. Criando-a agora...")
+    os.makedirs(pasta)
     return
 
   # Lista todos os arquivos da pasta 'documentos'
-  arquivos = [f for f in os.listdir(pasta) if f.endswith((".docx"))]
+  arquivos = [f for f in os.listdir(pasta) if f.endswith(".docx")]
 
-  print(f"--- Iniciando a leitura de {len(arquivos)} arquivos ---\n")
+  print(f"--- Processando {len(arquivos)} arquivos ---\n")
         
   for nome in arquivos:
     caminho_completo = os.path.join(pasta, nome)
@@ -21,19 +25,31 @@ def extrair_dados():
       # Abre o arquivo sem o usuário ver
       doc = Document(caminho_completo)
 
-      # Le o autor que você definiu no gerador.py
-      autor = doc.core_properties.author
-      titulo = doc.core_properties.title
+      # Cria um "Dicionário" com os dados do arquivo atual
+      dados_arquivo = {
+        "Arquivo": nome,
+        "Autor": doc.core_properties.author or "Não informado",
+        "Título": doc.core_properties.title or "Sem título"
+      }
 
-      print(f" Arquivo: {nome}")
-      print(f" Autor: {autor or 'Desconhecido'}")
-      print(f" Título: {titulo or 'Sem título'}")
-      print("-" * 30)
+      # Adiciona o dicionário na lista principal
+      lista_dados.append(dados_arquivo)
+      print(f" Lido: {nome}")
     
     except Exception as e:
-      print(f" Erro ao ler {nome}")
-      print(f" Detalhe técnico: {e}")
-      print("-" * 30)
+      print(f" Erro em {nome}: {e}")
+
+  # PANDAS
+  if lista_dados:
+    # Transforma a lista em uma tabela (DataFrame)
+    df = pd.DataFrame(lista_dados)
+
+    # Salva em um arquivo Excel
+    nome_excel = "relatorio_documentos.xlsx"
+    df.to_excel(nome_excel, index=False)
+    print(f"\n SUCESSO! Planilha '{nome_excel}' gerada com sucesso.")
+  else:
+    print("Nenhum dado extraído para gerar o Excel.")
 
 if __name__ == "__main__":
   extrair_dados()
